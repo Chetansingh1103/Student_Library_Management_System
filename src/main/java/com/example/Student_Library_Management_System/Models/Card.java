d61793ed5e09fe1bf9f48dcafbf1027626e6dd81
Chetan Singh <boyzingsing@gmail.com>
2023-02-13 16:10:39 +0530

    first

---
+++ src/main/java/com/example/Student_Library_Management_System/Models/Card.java
@@ -0,0 +1,82 @@
+package com.example.Student_Library_Management_System.Models;
+
+import com.example.Student_Library_Management_System.Enums.CardStatus;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "card")
+public class Card {
+
+    // plan is to save this card in db
+    // before saving we have to set its attributes : rule 1
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @CreationTimestamp // auto timestamp the time when an entry is created
+    private Date createdOn;
+
+    @UpdateTimestamp
+    private Date updatedOn; // sets time when entry is updated
+
+    @Enumerated(value = EnumType.STRING)
+    private CardStatus cardStatus;
+
+    @OneToOne
+    @JoinColumn
+    Student studentVariableName; // this variable is used in the parent class while doing bidirectional mapping
+
+    // Card is parent with respect to book class
+    @OneToMany(mappedBy = "card",cascade = CascadeType.ALL)
+    List<Book> bookIssued;
+
+    public Card() {
+        bookIssued = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Date getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(Date updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+    public Student getStudentVariableName() {
+        return studentVariableName;
+    }
+
+    public void setStudentVariableName(Student studentVariableName) {
+        this.studentVariableName = studentVariableName;
+    }
+
+    public CardStatus getCardStatus() {
+        return cardStatus;
+    }
+
+    public void setCardStatus(CardStatus cardStatus) {
+        this.cardStatus = cardStatus;
+    }
+}
---
+++ src/main/java/com/example/Student_Library_Management_System/Services/BookService.java
@@ -0,0 +1,50 @@
+package com.example.Student_Library_Management_System.Services;
+
+import com.example.Student_Library_Management_System.Models.Author;
+import com.example.Student_Library_Management_System.Models.Book;
+import com.example.Student_Library_Management_System.Repositories.AuthorRepository;
+import com.example.Student_Library_Management_System.Repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    AuthorRepository authorRepository;
+    public String addBook(Book book){
+
+        // we want to get the authorEntity
+        int authorId = book.getAuthor().getId();
+
+        // now we will be fetching the authorEntity
+
+        Author author = authorRepository.findById(authorId).get();
+
+        // how do exception handling
+
+        // basic attributes are already set from postman
+
+        // setting the foreign key attribute in the child class :
+
+        book.setAuthor(author);
+
+        // we need to update the list of book written in the parent class
+        List<Book> bookList = author.getBooksWritten();
+        bookList.add(book);
+        author.setBooksWritten(bookList);
+
+        // now the book is to be saved, but the author need to be updated
+
+        authorRepository.save(author); // date was modified
+
+        // save function works both  save function and as update function
+
+        // bookRepo.save is not required : because it will be autoCalled by cascading effect
+
+        return "book added successfully";
+
+    }
+}
